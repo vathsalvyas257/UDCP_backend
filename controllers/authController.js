ebdcdfa289f6cf917b76925dcb14449cd293cd73
vathsalvyas257 <saivathsalvyas257.gmail.com>
2025-03-07 21:51:39 +0530

    added cors env url

--- controllers/authController.js
+++ controllers/authController.js
@@ -165,6 +165,9 @@ module.exports.googleAuthCallback=async (req, res) => {
       maxAge: 60 * 60 * 1000, // 1-hour expiration
     });
     // Redirect user with token to frontend
+    if(process.env.NODE_ENV==='production'){
+        res.redirect("")
+    }
     res.redirect(`http://localhost:5173/auth-success?token=${token}`);
   }
 
