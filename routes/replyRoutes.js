028856e6dead337c36f084543adf540e1177ae18
vathsalvyas257 <saivathsalvyas257.gmail.com>
2025-03-07 23:19:30 +0530

    saved

--- routes/replyRoutes.js
+++ routes/replyRoutes.js
@@ -5,7 +5,7 @@ const authenticate = require("../middlewares/authenticate");
 const router = express.Router();
 
 // Add a reply to a thread (Protected)
-router.post("/:threadId", addReply);
+router.post("/:threadId", authenticate, addReply);
 
 // Get all replies for a thread (Public)
 router.get("/:threadId", getReplies);
